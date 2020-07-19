61f51eaacb22d0c053318f2ca2026179046c2fdc
Gabriel Mochi <gmochi56@icloud.com>
2020-07-19 02:02:10 -0300

    style: :art: eslint added

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,14 +1,12 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import colors from 'vuetify/lib/util/colors'
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import colors from 'vuetify/lib/util/colors';
 
-import '@mdi/font/css/materialdesignicons.css'
-
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdi'
+    iconfont: 'mdiSvg'
   },
   theme: {
     themes: {
@@ -32,4 +30,4 @@ export default new Vuetify({
       }
     }
   }
-})
+});
